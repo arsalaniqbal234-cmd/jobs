3d27ec364751a5e76a88f8c763e553b73eb37270
MUHAMMAD ARSLAN IQBAL <arsalaniqbal234@gmail.com>
2026-07-20 19:12:11 +0500

    addded cors frontendsadsad

--- app/main.py
+++ app/main.py
@@ -10,7 +10,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://jobsi-ten.vercel.app"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://jobsi-ten.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
