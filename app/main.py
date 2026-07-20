0195f72feb80e5a8401198f54fd1085d482a7b6e
MUHAMMAD ARSLAN IQBAL <arsalaniqbal234@gmail.com>
2026-07-20 19:34:23 +0500

    Fix CORS configuration

--- app/main.py
+++ app/main.py
@@ -13,11 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://jobsi-ten.vercel.app",
-        "http://localhost:3000",
-        "http://localhost:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
